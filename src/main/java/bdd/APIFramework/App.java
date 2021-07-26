c4b69ed9b48dc10e64136075e2a7d2b7d8f20606
Abhishek Chaturvedi <abhishektestnet@gmail.com>
2021-07-26 23:53:26 +0530

    changes in TestRunner file

--- src/main/java/bdd/APIFramework/App.java
+++ src/main/java/bdd/APIFramework/App.java
@@ -2,12 +2,9 @@ package bdd.APIFramework;
 
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
     }
 }
