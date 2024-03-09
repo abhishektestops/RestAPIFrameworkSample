2a41d0c1a33ff56e0f487fc04d9cade48ed9d8c3
Abhishek <abhishektestnet@gmail.com>
2024-03-09 12:13:35 +0530

    Fix for TestRunner file

--- src/test/java/cucumber/TestRunner.java
+++ src/test/java/cucumber/TestRunner.java
@@ -1,12 +1,14 @@
 package cucumber;
 
-
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "src/test/java/features", plugin = "json:target/jsonReports/cucumber-report.json", glue = {"stepDefinations"})
+@CucumberOptions(
+		features = "src/test/java/features", 
+		plugin = "json:target/jsonReports/cucumber-report.json", 
+		glue = {"stepDefinations" },
+		tags="@DeletePlace")
 public class TestRunner {
-//tags= {"@DeletePlace"}  compile test verify
 }
